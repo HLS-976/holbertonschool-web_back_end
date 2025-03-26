46f1a6fb159a87afa53c8875dd04f7ccd6f9be59
hls <hilliassmirali@gmail.com>
2025-03-26 13:33:44 +0100

    Add appendToEachArrayValue function to append a string to each element in an array

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/*eslint-disable*/
+export default function appendToEachArrayValue(array, appendString) {
+    let newArray = [];
+    for (let idx of array) {
+      newArray.push(appendString + idx)
+    }
+  
+    return newArray;
+  }
