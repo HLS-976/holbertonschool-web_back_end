a37ed41b4c0bfceabf298d6e900f9eddb2b85027
hls <hilliassmirali@gmail.com>
2025-03-27 10:14:49 +0100

    Refactor Currency class to improve code formatting and enforce string type checks for code and name properties

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,33 +1,31 @@
-/* eslint-disable */ 
-class Currency {
+/* eslint-disable */
+export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+      this._code = code;
+      this._name = name;
   }
 
   get code() {
-    return this._code;
+      return this._code;
   }
 
-  set code(value) {
-    if (typeof value !== 'string') {
-      throw TypeError ('code must be a string');
-    }
-    this._code = value;
+  set code(newCode) {
+      if (typeof newCode !== String)
+          throw TypeError('Code must be a string')
+      this._code = newCode;
   }
 
   get name() {
-    return this._name;
+      return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw TypeError ('name must be a string');
-    }
-    this._name = value;
+  set name(newName) {
+      if (typeof newName !== String)
+          throw TypeError('Name must be a string');
+      this._name = newName;
   }
 
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+      return `${this._name} (${this._code})`;
   }
 }
