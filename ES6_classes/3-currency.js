b20af51280d35c2e086850fdfae1601473b5374c
hls <hilliassmirali@gmail.com>
2025-03-27 10:04:46 +0100

    Remove type checks for code and name in Currency constructor

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,11 +1,5 @@
 class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') {
-      throw TypeError ('code must be a string');
-    }
-    if (typeof name !== 'string') {
-      throw TypeError ('name must be a string');
-    }
     this._code = code;
     this._name = name;
   }
