05270037fa709ca7141f817aa1a4c008521d4496
hls <hilliassmirali@gmail.com>
2025-03-28 21:57:01 +0100

    Add ES6 classes for Car, Airport, HolbertonClass, and StudentHolberton with appropriate properties and methods

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,23 +1,18 @@
+/* eslint-disable */
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if(this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage')
     }
-    this.sqft = sqft;
-  }
-
-  static checkNumber(num) {
-    if (typeof num !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    return num;
-  }
-
-  set sqft(newSqft) {
-    this._sqft = Building.checkNumber(newSqft);
+    
+    this._sqft = sqft;
   }
 
   get sqft() {
-    return this._sqft;
+      return this._sqft;
+  }
+
+  set sqft(new_sqft) {
+      this._sqft = new_sqft;
   }
 }
