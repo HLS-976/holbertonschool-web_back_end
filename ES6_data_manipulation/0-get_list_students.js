c9a1cd7614b4a6f6e67374f062b0d15a8475ce96
hls <hilliassmirali@gmail.com>
2025-04-02 10:38:50 +0200

    Fix property name from 'firstname' to 'firstName' in getListStudents function

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export default function getListStudents() {
   return [
-    { id: 1, firstname: 'Gillaume', location: 'San Francisco' },
-    { id: 2, firstname: 'James', location: 'Columbia' },
-    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+    { id: 1, firstName: 'Gillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
