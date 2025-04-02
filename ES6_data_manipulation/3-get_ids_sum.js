e8ce7e19960889224354ab04a6dac8b5cc81fc36
hls <hilliassmirali@gmail.com>
2025-04-02 11:16:10 +0200

    Return an empty array for non-array input in getStudentIdsSum function

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,4 +2,5 @@ export default function getStudentIdsSum(array) {
   if (array instanceof Array) {
     return array.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
   }
+  return 0;
 }
