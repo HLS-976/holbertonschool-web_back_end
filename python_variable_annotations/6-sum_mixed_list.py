32b08466786ba51e85229d7276be30a8c2f00564
hls <hilliassmirali@gmail.com>
2025-04-22 15:33:00 +0200

    Refactor type hint in sum_mixed_list function for improved clarity

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module provides the sum_mixed_list function"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     This function return the sum of mixed list
     """
