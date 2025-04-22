a6a32b1b4162e3b88873505270615a6d9376b03a
hls <hilliassmirali@gmail.com>
2025-04-22 15:28:27 +0200

    Add sum_mixed_list function to calculate the sum of a mixed list of integers and floats

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module provides the sum_mixed_list function"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    """
+    This function return the sum of mixed list
+    """
+    return sum(mxd_lst)
