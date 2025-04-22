8f7caefd2c8172519ea03bd9780555acf2886fd5
hls <hilliassmirali@gmail.com>
2025-04-22 16:07:59 +0200

    Add make_multiplier function to return a callable that multiplies a float by a given multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module provides the function make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """This function returns a function that multiplies a float by the multiplier"""
+    return lambda x: x * multiplier
