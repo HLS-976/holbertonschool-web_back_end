7bedcd04243a64694037fd5eb67156a2bcce7b1a
hls <hilliassmirali@gmail.com>
2025-04-22 16:19:50 +0200

    Fix return type annotation in element_length function to return a list of tuples

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module provides the elemnet_length function"""
-from typing import List, Union, Iterator
+from typing import List, Union, Tuple
 
 
-def element_length(lst: List[Union[int, float, str]]) -> List[Iterator[int, int]]:
+def element_length(lst: List[Union[int, float, str]]) -> List[Tuple[str, int]]:
     return [(i, len(i)) for i in lst]
