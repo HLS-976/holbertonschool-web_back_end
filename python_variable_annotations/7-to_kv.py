12e591fbf55b379127103a1797d5e1f69f571e38
hls <hilliassmirali@gmail.com>
2025-04-22 15:40:23 +0200

    Refactor type hint in to_kv function for improved type clarity

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module provides the to_kv function"""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple:
+def to_kv(k: str, v: Union[int | float]) -> Tuple:
     """
     This function returns a tuple of two elements
     """
