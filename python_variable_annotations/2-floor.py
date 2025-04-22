a509ade7197165716c72b9b4e859ea394a39b388
hls <hilliassmirali@gmail.com>
2025-04-22 15:06:08 +0200

    Add floor function to return the integer part of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module provides the floor function"""
+
+
+def floor(n: float) -> int:
+    """
+    This function return the entire division of a float
+    """
+    return int(n)
