35d7637657b4af0af3958b6eaf98ae906f5358de
hls <hilliassmirali@gmail.com>
2025-04-22 14:40:49 +0200

    Add add function to perform the sum of two numbers

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module provides a add function"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function return the sum of two numbers
+    """
+    return a + b
