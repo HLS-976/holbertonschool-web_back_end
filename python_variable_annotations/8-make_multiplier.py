45a5c680d012d358ee0a0670209f70037265722b
hls <hilliassmirali@gmail.com>
2025-04-22 16:08:51 +0200

    Refactor docstring in make_multiplier function for improved readability

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,5 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """This function returns a function that multiplies a float by the multiplier"""
+    """
+    This function returns a function 
+    that multiplies a float by the multiplier
+    """
     return lambda x: x * multiplier
