e3ba1556b65943c07dc26e5044d5ec0e0fe1da46
hls <hilliassmirali@gmail.com>
2025-04-28 11:21:14 +0200

    Fix async_generator to return an AsyncGenerator type and yield random numbers

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,11 @@
 """This module provides a coroutines"""
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator:
     """Asynchronously generates random numbers."""
     for _ in range(10):
         await asyncio.sleep(1)
-        return random.uniform(0, 10)
+        yield random.uniform(0, 10)
