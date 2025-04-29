131627828e0d5e5a908597a62cb5f6e902323780
hls <hilliassmirali@gmail.com>
2025-04-29 13:40:51 +0200

    Refactor async_generator to use Generator type and improve module documentation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""This module provides a coroutines"""
-import random
+"""
+Module 0-async_generator - Async Generator
+"""
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
+import random
 
 
-async def async_generator() -> AsyncGenerator:
-    """Asynchronously generates random numbers."""
-    for _ in range(10):
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Async generator that yields random numbers between 0 and 10
+    """
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
