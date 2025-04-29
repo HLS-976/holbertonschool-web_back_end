d4039f28df7bd4372d49824f092411c3536f0386
hls <hilliassmirali@gmail.com>
2025-04-29 13:57:19 +0200

    Refactor async_comprehension module documentation and fix async_generator import typo

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-"""This module provides a coroutines named async_comprehension"""
-async_genrator = __import__("0-async_generator").async_genrator
+"""
+Module 1-async_comprehension - Async Comprehension
+"""
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """This function returns a list of random numbers"""
-    return [i async for i in async_genrator()]
+    """
+    This function return a list of random numbers given by async_generator
+    """
+    return [i async for i in async_generator()]
