d97d3391c18fdee416d734817f1d5dc8401fc482
hls <hilliassmirali@gmail.com>
2025-05-02 18:44:12 +0200

    Refactor log_stats function to improve clarity and consistency in output

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
-"""Module for log_stats function"""
+"""
+Provides stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    """provides some stats about Nginx logs stored in MongoDB"""
-    client = MongoClient('localhost', 27017)
+    """Prints statistics about Nginx logs in MongoDB"""
+    client = MongoClient()
     db = client.logs
     collection = db.nginx
 
-    logs_qty = collection.count_documents({})
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
     status_check = collection.count_documents({
         "method": "GET",
         "path": "/status"
         })
-
-    print(f"{logs_qty} logs")
-    print("Methods:")
-    for method in methods:
-        method_qty = collection.count_documents({"method": method})
-        print(f"\t{method}: {method_qty}")
     print(f"{status_check} status check")
 
 
