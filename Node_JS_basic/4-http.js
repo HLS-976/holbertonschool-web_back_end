d92ccf992cb4c45515de19d4a227a55253efafca
hls <hilliassmirali@gmail.com>
2025-05-23 18:05:16 +0200

    Simplify response handling by combining write and end methods in HTTP server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,8 +3,7 @@ const http = require('http');
 const app = http.createServer((request, response) => {
   response.setHeader('Content-Type', 'text/plain');
   response.statusCode = 200;
-  response.write('Hello Holberton School!');
-  response.end();
+  response.end('Hello Holberton School!');
 });
 
 app.listen(1245, 'localhost', () => {
