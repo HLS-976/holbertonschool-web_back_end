882fc68ae193390c8d82e962ea38a02625ef6e0c
hls <hilliassmirali@gmail.com>
2025-05-23 20:47:18 +0200

    Refactor countStudents function to improve readability and handle empty lines

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,26 +1,25 @@
 const fs = require('fs').promises;
 
-async function countStudents(filePath) {
+async function countStudents(path) {
   try {
-    const data = await fs.readFile(filePath, 'utf-8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    if (lines.length <= 1) {
-      console.log('Number of students: 0');
-      return;
-    }
-    const students = lines.slice(1);
+    const data = await fs.readFile(path, 'utf8');
+    const rows = data.trim().split('\n').slice(1);
     const fields = {};
 
-    students.forEach((student) => {
-      const [firstName, , , field] = student.split(',');
-      if (!fields[field]) {
-        fields[field] = [];
+    rows.forEach((row) => {
+      const columns = row.split(',');
+      const [firstname, , , field] = columns;
+      if (firstname && field) {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
       }
-      fields[field].push(firstName);
+      fields[field].push(firstname);
     });
-    console.log(`Number of students: ${students.length}`);
-    for (const [field, names] of Object.entries(fields)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+
+    console.log(`Number of students: ${rows.length}`);
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
     }
   } catch (err) {
     throw new Error('Cannot load the database');
