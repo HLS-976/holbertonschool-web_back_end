834e30a054b3ab705faf81d3072b4a3d06de1dbc
hls <hilliassmirali@gmail.com>
2025-05-23 09:51:16 +0200

    Refactor HTTP server response handling to always send a greeting message

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,15 +1,13 @@
 const http = require('http');
 
 const app = http.createServer((request, response) => {
-  if (request.url) {
-    response.setHeader('Content-Type', 'text/plain');
-    response.write('Hello Holberton School!');
-    response.end();
-  }
+  response.setHeader('Content-Type', 'text/plain');
+  response.write('Hello Holberton School!');
+  response.end();
 });
 
 app.listen(1245, 'localhost', () => {
-  console.log('Adresse: http://localhost:1245/');
+  console.log('Adresse: http://localhost:1245');
 });
 
 module.export = app;
